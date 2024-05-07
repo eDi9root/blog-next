2b4f73ce4cf9ab9043e457c8822c125a3965cd54
eDi9root <119016575+eDi9root@users.noreply.github.com>
2024-05-06 23:51:20 -0400

    fix: route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,38 +1,48 @@
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-import { type CookieOptions, createServerClient } from '@supabase/ssr'
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { Database } from "@/lib/types/supabase";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get('code')
-  // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get('next') ?? '/'
+	const requestUrl = new URL(request.url);
+	const isAuth = cookies().get("supabase-auth-token");
 
-  if (code) {
-    const cookieStore = cookies()
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          get(name: string) {
-            return cookieStore.get(name)?.value
-          },
-          set(name: string, value: string, options: CookieOptions) {
-            cookieStore.set({ name, value, ...options })
-          },
-          remove(name: string, options: CookieOptions) {
-            cookieStore.delete({ name, ...options })
-          },
-        },
-      }
-    )
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
-    }
-  }
+	if (isAuth) {
+		return NextResponse.redirect(requestUrl.origin);
+	}
 
-  // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+	const { searchParams } = new URL(request.url);
+	const code = searchParams.get("code");
+	const next = searchParams.get("next") ?? "/";
+
+	if (code) {
+		const cookieStore = cookies();
+		const supabase = createServerClient<Database>(
+			process.env.NEXT_PUBLIC_SUPABASE_URL!,
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+			{
+				cookies: {
+					get(name: string) {
+						return cookieStore.get(name)?.value;
+					},
+					set(name: string, value: string, options: CookieOptions) {
+						cookieStore.set({ name, value, ...options });
+					},
+					remove(name: string, options: CookieOptions) {
+						cookieStore.set({ name, value: "", ...options });
+					},
+				},
+			}
+		);
+
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+		if (!error) {
+			return NextResponse.redirect(requestUrl.origin + next);
+		}
+	} else {
+		console.log("no code?");
+	}
+
+	return NextResponse.redirect(requestUrl.origin + "/auth/error");
 }
